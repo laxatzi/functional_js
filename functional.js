ea9fc80e932d9349e5a856a4a8205db11215194b
laxatzi <laxatzi@yahoo.com>
2020-02-05 15:22:26 +0200

    delete function.js and init code in imperativeProg.js file

--- functional.js
+++
@@ -1,7 +0,0 @@
-//Functiona Programming: The Hazards of using imperative code
-
-/* 
-   Functional programming keeps your code easy to manage, and saves you from sneaky bugs. But before we get there lets look at an imperative approach to programming to highlight where you may have issues.
-
-   An imperative style in programming 
-*/
--- imperativeProg.js
+++ imperativeProg.js
@@ -1,1 +1,8 @@
+//Functiona Programming: The Hazards of using imperative code
+
+/* 
+   Functional programming keeps your code easy to manage, and saves you from sneaky bugs. But before we get there lets look at an imperative approach to programming to highlight where you may have issues.
+
+   An imperative style in programming 
+*/
 
