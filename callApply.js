dfa7e11ec4b3d3a6c9dedd4bc195d1ad71a4735a
laxatzi <laxatzi@yahoo.com>
2020-02-10 16:26:36 +0200

    update callApply.js and test.html files

--- callApply.js
+++ callApply.js
@@ -1,1 +1,26 @@
+/* 
+   The 'call()' method allows a function to be called by an object that is provided as the first arg.
+   Inside the body function, the keyword 'this' is used to refer to the object function is called on.
+*/
 
+function sayHi(){
+   return "Hi "+ this.name;
+}
+
+const man = {name:"Theophilos"};
+const woman = {name: "Theodora"};
+
+console.log(sayHi.call(woman)); // Hi Theodora
+console.log(sayHi.call(man));  // Hi Theophilos
+
+// If the function has arguments, these arguments are given in the same order "after" the first argument
+
+function greet(greeting) {
+   return greeting + " " + this.name;
+}
+
+console.log(greet.call(woman, "Hello")); // Hello Theodora
+
+console.log(greet.call(null, "Aris"));
+
+console.log(sayHi.apply( ["Theophilos"]));
