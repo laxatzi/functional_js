0e96d393fe02336ed52045ab777050ac8bbf53e4
laxatzi <laxatzi@yahoo.com>
2020-02-26 15:13:36 +0200

    implement filter on a Prototype

--- fn_ex8.js
+++ fn_ex8.js
@@ -45,6 +45,8 @@
                                ] */
 }());//end wrapper iife 
 
+// 
+
 // Exercise:
 /* 
       The variable 'watchList' holds an array of objects with information on several movies. 
@@ -66,7 +68,8 @@
          "Runtime": "148 min",
          "Genre": "Action, Adventure, Crime",
          "Director": "Christopher Nolan",
-         "Writer": "Christopher Nolan"
+         "Writer": "Christopher Nolan",
+         "imdbRating": "8.8"
       },
       {
          "Title": "Interstellar",
@@ -77,6 +80,7 @@
          "Genre": "Adventure, Drama, Sci-Fi",
          "Director": "Christopher Nolan",
          "Writer": "Jonathan Nolan, Christopher Nolan",
+         "imdbRating": "9.2"
    },
    {
         "Title": "The Dark Knight",
@@ -86,15 +90,66 @@
          "Runtime": "152 min",
          "Genre": "Action, Adventure, Crime",
          "Director": "Christopher Nolan",
-         "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay)
+         "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay)",
+         "imdbRating": "9.0"
    }
 ]; // end of array   
 
     // add your code below this line
 
-         var filteredList;
+    const  filteredList = watchList.filter(movie => {
+       return parseFloat(movie.imdbRating) >= 9.0;
+    })
+    .map(movie => ({
+       title: movie["Title"],
+       rating: movie["imdbRating"]
+    }))
     // add your code above this line
 
-    console.log(filteredLIst);
+    console.log(filteredList);  /* 
+                                    logs:
+                        [
+                           [object Object] {
+                           rating: "9.2",
+                           title: "Interstellar"
+                           }, 
+                           [object Object] {
+                           rating: "9.0",
+                           title: "The Dark Knight"
+                           }
+                        ]
+    */
+
+}());//end wrapper iife
+
+// Implement a Filter Method on a Prototype
+/* 
+    It would teach us a lot about the filter method if we try to implement a version of it that behaves
+    exactly like 'Array.prototype.filter()'. 
+    It can use either a 'for' loop or 'Array.prototype.forEach()' 
+
+    NOTE: A pure function is allowed to alter local variables defined within its scope, although, it is preferable to avoid that as well.
+
+    Exercise: 
+        Write your own 'Array.prototype.myFilter()' which should behave exactly like 'Array.prototype.filter()'. You may use a for loop or the 'forEach' method.
+*/
+
+(function(){
+   'use strict';
+
+   let s = [23, 65, 98, 5];
+
+   Array.prototype.myFilter = function(callback){
+      var newArray = [];
+      // Add your code below this line
+         
+      // Add your code above this line
+      return newArray;
+    
+    };
+    
+    var new_s = s.myFilter(function(item){
+      return item % 2 === 1;
+    });
 
 }());//end wrapper iife
