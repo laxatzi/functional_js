cf12e8d33f08eba4bf5d940af69abf2dbc86fa93
laxatzi <laxatzi@yahoo.com>
2020-02-28 16:12:09 +0200

    update concat.js file

--- concat.js
+++ concat.js
@@ -26,3 +26,38 @@
   }
 
 }());//end wrapper iife
+
+// Add Elements to the End of an Array Using concat Instead of push
+/* 
+    Functional programming is all about creating and using non-mutating functions.
+
+    The last challenge introduced the 'concat' method as a way to combine arrays into a new one
+    without mutating the original arrays. Compare 'concat' to the 'push' method. 'Push' adds an item 
+    to the end of the same array it is called on, which mutates that arrays. 
+    Here is an example: 
+
+*/
+
+(function(){
+  'use strict';
+  var arr = [1,2,3];
+  arr.push([4,5,6]);
+
+}());//end wrapper iife
+
+/* 
+  Change the 'nonMutatingPush' function so it uses 'concat' to add 'newItem' to the end of 'original' instead of 'push'. The function should return an array
+*/
+
+(function(){
+  'use strict';
+  function nonMutatingPush(original, newItem){
+     // add code below this line
+        
+     // add code above this line
+  }
+
+  var first = [1,2,3];
+  var second = [4,5];
+  console.log(nonMutatingPush(first, second));
+}());//end wrapper iife
