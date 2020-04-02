ea81d14c37e8161c026a08da1a951a35900613e7
laxatzi <laxatzi@yahoo.com>
2020-04-02 15:40:11 +0300

    resolve functional programming exercises

--- practiseFCC.js
+++ practiseFCC.js
@@ -9,7 +9,6 @@ const watchList = [
      "Year": "2010",
      "Genre": "Action, Adventure, Crime",
      "Director": "Christopher Nolan",
-     "Writer": "Christopher Nolan",
      "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
      "imdbRating": "8.8"
    },
@@ -18,7 +17,6 @@ const watchList = [
      "Year": "2014",
      "Genre": "Adventure, Drama, Sci-Fi",
      "Director": "Christopher Nolan",
-     "Writer": "Jonathan Nolan, Christopher Nolan",
      "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
      "imdbRating": "8.6"
    },
@@ -27,18 +25,16 @@ const watchList = [
      "Year": "2008",
      "Genre": "Action, Adventure, Crime",
      "Director": "Christopher Nolan",
-     "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
      "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
-     "imdbRating": "9.0",
+     "imdbRating": "9.0"
    },
    {
      "Title": "Batman Begins",
      "Year": "2005",
      "Genre": "Action, Adventure",
      "Director": "Christopher Nolan",
-     "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
      "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
-     "imdbRating": "8.3",
+     "imdbRating": "8.3"
     }
 ];
   // Old code
@@ -56,3 +52,75 @@ const watchList = [
 console.log(JSON.stringify(ratings));
  
 }// end of block
+
+//The variable watchList holds an array of objects with information on several movies. Use a combination of filter and map on watchList to assign a new array of objects with only title and rating keys. The new array should only include objects where imdbRating is greater than or equal to 8.0. Note that the rating values are saved as strings in the object and you may need to convert them into numbers to perform mathematical operations on them.
+
+{
+   'use strict';
+   
+   const watchList = [
+      {
+        "Title": "Inception",
+        "Year": "2010",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+        "imdbRating": "8.8"
+      },
+      {
+        "Title": "Interstellar",
+        "Year": "2014",
+        "Genre": "Adventure, Drama, Sci-Fi",
+        "Director": "Christopher Nolan",
+        "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+        "imdbRating": "8.4"
+      },
+      {
+        "Title": "The Dark Knight",
+        "Year": "2008",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+        "imdbRating": "9.0"
+      },
+      {
+        "Title": "Batman Begins",
+        "Year": "2005",
+        "Genre": "Action, Adventure",
+        "Director": "Christopher Nolan",
+        "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+        "imdbRating": "8.3"
+       }
+   ];
+
+// Only change code below this line
+ //  const filteredList;
+// Only change code above this line
+
+// solution
+ const  filteredList = watchList.map( movie=> ({
+   title: movie["Title"],
+   rating: movie["imdbRating"]
+}) ).filter(movie=> movie.rating >= parseFloat("8.5"));
+
+// test
+ console.log(filteredList);
+ 
+}
+
+// Rewrite the function "nonMutatingSplice" by using "slice" instead of "splice".
+// It should limit the provided 'cities' array to a length of 3, and return a new array with only the first three items.
+
+// Do not mutate the original array provided to the function
+
+{
+   'use strict';
+
+   function nonMutatingSplice(cities) {
+      // Only change code below this line
+        return cities.slice(0,3);
+      // Only change code above this line
+    }
+    const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];    
+    console.log(nonMutatingSplice(inputCities));
+}
